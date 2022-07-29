245c47ff6b1e3c26fd7f72bc87ce9403c5d0d941
Gustavo Okamoto <gustavo_okamoto@pop-os.localdomain>
2022-07-29 10:58:32 -0300

    implementanto componente de navBar mas precisa ser alterado e configurado

--- main_folder/cadastro/src/routes.js
+++ main_folder/cadastro/src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './loginComp/login';
 import Wallet from './walletComp/wallet'
 import Sign from './signComp/sign';
+import NavBar from './navBarComp/navBar'
 // import Perfil from './perfil/perfil'
 // import Inicio from './inicio/inicio'
 
@@ -12,6 +13,7 @@ function MainRoutes (){
             <Route path="/" element={<Login />} />
             <Route path="/signUp" element={<Sign />} />
             <Route path="/wallet" element={<Wallet />} />
+            <Route path="/navBar" element={<NavBar />} />
             {/* <Route path="/user" element={<User />} /> */}
             {/* <Route path="/menu" element={<Menu />} /> */}
         </Routes>
