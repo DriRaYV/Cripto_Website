16637009692f28be53c6cc00f67a003d22b911eb
Rafaella Guimarães Venturini <99514290+DriRaYV@users.noreply.github.com>
2022-07-30 11:43:52 -0300

    Update sign.js

--- main_folder/cadastro/src/signComp/sign.js
+++ main_folder/cadastro/src/signComp/sign.js
@@ -1,34 +1,32 @@
 import './sign.css';
-import { set, useForm } from "react-hook-form";
-import { useRef, useState, useLayoutEffect } from 'react';
-import { registerUser } from './registrerUser';
+import {  useForm } from "react-hook-form";
+import {  useState } from 'react';
 
 function Sign() {
 
-  const [username, setUsername] = useState()
-  const [password, setPassword] = useState()
-  const [password2, setPassword2] = useState()
-  const [cep, setCep] = useState()
-  const [address, setAddress] = useState()
-  const [number, setNumber] = useState()
-  const [district, setDistrict] = useState()
-  const [city, setCity] = useState()
-  const [uf, setUf] = useState()
+  var [username, setUsername] = useState()
+  var [password, setPassword] = useState()
+  var [password2, setPassword2] = useState()
+  var [cep, setCep] = useState()
+  var [address, setAddress] = useState()
+  var [number, setNumber] = useState()
+  var [district, setDistrict] = useState()
+  var [city, setCity] = useState()
+  var [uf, setUf] = useState()
 
 
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const { register, handleSubmit,setValue } = useForm();
 
   function GetData(userData) {
-    const signName = (`${userData.name}`)
-    const signUserName = (`${userData.userName}`)
-    const signPassword = (`${userData.password}`)
-    const signConfirmPassword = (`${userData.confirmPassword}`)
-    const signCep = (`${userData.cep}`)
-    const signAddress = (`${userData.address}`)
-    const signNumber = (`${userData.number}`)
-    const signDistrict = (`${userData.district}`)
-    const signCity = (`${userData.city}`)
-    const signUf = (`${userData.uf}`)
+    var signUserName = (`${userData.userName}`)
+    var signPassword = (`${userData.password}`)
+    var signConfirmPassword = (`${userData.confirmPassword}`)
+    var signCep = (`${userData.cep}`)
+    var signAddress = (`${userData.address}`)
+    var signNumber = (`${userData.number}`)
+    var signDistrict = (`${userData.district}`)
+    var signCity = (`${userData.city}`)
+    var signUf = (`${userData.uf}`)
 
     setUsername(signUserName)
     setPassword(signPassword)
@@ -39,23 +37,12 @@ function Sign() {
     setDistrict(signDistrict)
     setCity(signCity)
     setUf(signUf)
+
+
   }
 
-  const passwd2 = useRef()
-
-
-  // function verifyPassword(){
-  //   if(password === "" && password2 === ""){
-  //     alert ('As senhas não podem ser vazias')
-  //   }
-  //   if(password !== password2){
-  //     alert('As senhas são diferentes! Tente Novamente')
-  //   }
-
-  // }
-
-  function verifySigin() {
-    fetch("https://musicapig.herokuapp.com/users/login", {
+    const verifyUsername = () => {
+      fetch("https://musicapig.herokuapp.com/users/exists ", {
       headers: {
         'Content-Type': 'application/json'
       },
@@ -64,80 +51,66 @@ function Sign() {
         username: username
       })
     }).then(response => verifyLogin(response))
-  }
-  async function verifyLogin(response) {
-    const { message } = await response.json()
-    const { status } = response
 
-    if (status === 500) {
-      alert(`erro no servidor: ${message}`)
-    } else if (status === 401) {
-      alert(message)
-    } else if (status === 200) {
-      alert(message)
+    async function verifyLogin(response) {
+      const { message } = await response.json()
+      const { status } = response
+  
+      if (status === 500) {
+        alert(`erro no servidor: ${message}`)
+      } else if (status === 401) {
+        alert(message)
+      } else if (status === 200) {
+        alert(message)
+      }
     }
+    }
+
+    const submitData = () =>{
+      
+      fetch('https://musicapig.herokuapp.com/users', {
+          method: "POST",
+          headers:{
+              'Content-Type':"application/json"
+          },
+          body:JSON.stringify({
+              username: username,
+              password_user:password, 
+              user_address:cep,
+              cep: address,
+              house_number  : number,
+              uf: uf,
+              district: district, 
+              city: city
+  
+          })
+      }).then(alert('usuário registrado'))
   }
 
-  function siginData() {
-    const data = JSON.stringify({
-      username: username,
-      senha: password,
-      cep: cep,
-      rua: address,
-      number: number,
-      uf: uf,
-      district: district,
-      city: city
+  const checkPassword = () =>{
+    if(password === "" && password2 === ""){
+      alert ('As senhas não podem ser vazias')
+    }
+    else if(password !== password2){
+      alert('As senhas são diferentes! Tente Novamente')
+    }
+
+    else{
+      return true
+    }
+
+  }
+
+  const checkCEP = (e) => {
+    const cep = e.target.value.replace(/\D/g, '');
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then(res => res.json()).then(data => {
+      setValue('address', data.logradouro)
+      setValue('district',data.bairro)
+      setValue('city',data.localidade)
+      setValue('uf',data.uf)
     })
-    registerUser(data)
   }
-
-  function limpa_formulário_cep() {
-    address = ("");
-    district = ("");
-    city = ("");
-    uf = ("");
-  }
-  function meu_callback(conteudo) {
-    if (!("erro" in conteudo)) {
-      address = (conteudo.logradouro);
-      district = (conteudo.bairro);
-      city = (conteudo.localidade);
-      uf = (conteudo.uf);
-    }
-    else {
-      limpa_formulário_cep();
-      alert("CEP não encontrado.");
-    }
-  }
-
-  function pesquisacep(valor) {
-    var cep = valor.replace(/\D/g, '');
-
-    if (cep != "") {
-
-      var validacep = /^[0-9]{8}$/;
-
-      if (validacep.test(cep)) {
-
-        address = "...";
-        district = "...";
-        city = "...";
-        uf = "...";
-
-        fetch('https://viacep.com.br/ws/' + cep + '/json').then(responde => responde.json()).then(response => meu_callback(response))
-
-
-      }
-      else {
-        limpa_formulário_cep();
-        alert("Formato de CEP inválido.");
-      }
-    }
-    else {
-      limpa_formulário_cep();
-    }
-  };
 
 
   return (
@@ -145,28 +118,28 @@ function Sign() {
       <div id='light-background'>
         <h1 id='title'>Vamos cadastrar o seu perfil?</h1>
 
-        <form onSubmit={handleSubmit(GetData)}>
+        <form onChange={handleSubmit(GetData)}>
           <div id='information'>
             <div id='left-information'>
               <div className='div-label'>
                 <label>
                   Insira um nome de usuário:
                   <div className='div-input'>
-                    <input type="text" className='input-left' {...register("userName")} onBlur={verifySigin()} />
+                    <input type="text" className='input-left' {...register("userName")} onBlur={verifyUsername} />
                   </div>
                 </label>
 
                 <label>
                   Insira uma senha:
                   <div className='div-input'>
-                    <input type="password" className='input-left' {...register("password")} />
+                    <input type="password" className='input-left' {...register("password")}/>
                   </div>
                 </label>
 
                 <label>
                   Repita a senha:
                   <div className='div-input'>
-                    <input type="password" className='input-left' id='input-passwd2' {...register("confirmPassword")} ref={passwd2} />
+                    <input type="password" className='input-left' id='input-passwd2' {...register("confirmPassword")} onBlur={checkPassword} />
                   </div>
                 </label>
               </div>
@@ -178,7 +151,7 @@ function Sign() {
                   <label>
                     Insira o CEP:
                     <div className='div-input2'>
-                      <input type="text" className='input-right' {...register("cep")} />
+                      <input type="text" className='input-right' {...register("cep")} onBlur={checkCEP}/>
                     </div>
                   </label>
                 </div>
@@ -226,7 +199,7 @@ function Sign() {
             </div>
           </div>
           <div id='div-button'>
-            <button type='submit' id='botao' onClick={siginData()}>CADASTRE-SE</button>
+            <button type='submit' id='botao' onClick={submitData}>CADASTRE-SE</button>
           </div>
         </form>
       </div>
@@ -235,3 +208,5 @@ function Sign() {
 }
 
 export default Sign;
+
+
