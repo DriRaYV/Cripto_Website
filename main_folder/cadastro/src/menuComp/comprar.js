abc917295642bf19bf6a9b8d4b40fd201e3d212d
Gustavo Okamoto <gustavookamoto1945@gmail.com>
2022-08-13 12:36:18 -0300

    tentativa de arrumar os erros do git e subir

--- main_folder/cadastro/src/menuComp/comprar.js
+++ main_folder/cadastro/src/menuComp/comprar.js
@@ -43,51 +43,7 @@ function Comprar() {
     }
   }
 
-  return (
-<<<<<<< HEAD
-    <div id='fundo-black'>
-      <div id='fundo-light'>
-        <h1 id='title-comprar'>
-            Vamos Comprar?<br/> 
-        </h1>
-
-        <form >
-          <div id='information2-comprar'>
-                <div id='information3-comprar'>
-                    <div className='label-div-comprar'>
-                        <label>
-                           Selecione a opção de compra
-                            <div className='input-div-comprar'>
-                                <select>
-                                  <option>Bitcoin</option>
-                                  <option>Ethereun</option>
-                                  <option>Tether</option>
-                                  <option>USD</option>
-                                  <option>USDC</option>
-                                  <option>Binance Coin</option>
-                                  <option>Binance USD</option>
-                                  <option>HEX</option>
-                                  <option>Cardano</option>
-                                  <option>XRP</option>
-                                  <option>Solana</option>
-                                </select>
-                            </div>
-                        </label>
-                        <br/>
-                        <label>
-                            Quantidade
-                            <div className='input-div-comprar'>
-                                <input type="password" className='inputSenha-comprar'/>
-                            </div>
-                        </label>
-                    </div>
-                </div>
-            </div>
-            <div id='div-button-comprar'>
-                <button type='submit' id='button2-comprar' >Comprar</button>
-            </div>
-            <h3>Acessar Minha<Link to='/carteira'>Carteira</Link></h3>
-=======
+  return(
     <div id="fundo-escuro">
       <div id="fundo-claro">
         <h1 id="title">
@@ -165,11 +121,10 @@ function Comprar() {
           <h3>
             Acessar Minha<Link to="/carteira">Carteira</Link>
           </h3>
->>>>>>> 3d4dc253bd98b20087dd5f499cbb4da06678b604
         </form>
       </div>
     </div>
-  );
+)
 }
 
 export default Comprar;
