bdde7be8d48f838e7e40ff7c9a2dc988f7c9dabf
Gustavo Okamoto <gustavookamoto1945@gmail.com>
2022-08-14 22:17:41 -0300

    consertando os links da navBar

--- main_folder/cadastro/src/routes.js
+++ main_folder/cadastro/src/routes.js
@@ -3,8 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './loginComp/login';
 import Wallet from './walletComp/wallet'
 import Sign from './signComp/sign';
-import NavBar from './navBarComp/navBar'
- import User from './userComp/App'
+import User from './userComp/App'
 import Menu from './menuComp/menu'
 import Comprar from './menuComp/comprar'
 
@@ -14,7 +13,6 @@ function MainRoutes (){
             <Route path="/" element={<Login />} />
             <Route path="/cadastro" element={<Sign />} />
             <Route path="/carteira" element={<Wallet />} />
-            <Route path="/barra-de-navegação" element={<NavBar />} />
             <Route path="/usuario" element={<User />} />
             <Route path="/inicio" element={<Menu/>} /> 
             <Route path="/comprar" element={<Comprar/>} /> 
