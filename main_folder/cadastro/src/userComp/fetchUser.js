fd2bbf1be082df76c488897d7f370e28cf98c6bf
Rafaella Guimarães Venturini <drirayv@pop-os.localdomain>
2022-08-15 11:37:07 -0300

    fegy

--- main_folder/cadastro/src/userComp/fetchUser.js
+++ main_folder/cadastro/src/userComp/fetchUser.js
@@ -4,27 +4,35 @@ import axios from "axios";
 
 function DataUser() {
   const token = sessionStorage.getItem('token')
-  const axiosConfig = {
-    headers: {
-      'Content-Type': 'application/json'
-    },
-  }
+
   const [userData, SetUserData] = useState("");
   useEffect(() => {
-    axios.post("https://musicapig.herokuapp.com/users/perfil",{token:token},axiosConfig).then((res) => {
-      const resp = res.data;
-      SetUserData(resp);
-    });
-  });
 
+    fetch("https://musicapig.herokuapp.com/users/perfil", {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: "POST",
+      body: JSON.stringify({
+      "token":token
+      }),
+    }).then(res => res.json())
+    .then((response) => {
+      let res = response.userInfo[0]
+      SetUserData(res)
+      console.log(res)
+    });
+  }
+  );
+  
   function fetchUser() {
     return userData;
   }
   return (
     <>
       <p className="paragraph">{fetchUser().username}</p>
-      <p className="paragraph">{fetchUser().cep}</p>
       <p className="paragraph">{fetchUser().user_address}</p>
+      <p className="paragraph">{fetchUser().cep}</p>
       <p className="paragraph">{fetchUser().house_number}</p>
       <p className="paragraph">{fetchUser().district}</p>
       <p className="paragraph">{fetchUser().city}</p>
